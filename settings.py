66d786f73b9b7212759dbb4fac1d3693a458c29e
Katharine Berry <katharine@getpebble.com>
2014-09-04 13:53:03 -0700

    Try changing to ip.pebble-sockets.com.

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
 TTL = 525600 * 60
-ROOT_DOMAIN = "pebble-sockets.com"
+ROOT_DOMAIN = "ip.pebble-sockets.com"
 MNAME = "ns1.pbsock.getpebble.com."
 RNAME = "cloudpebble.getpebble.com."
 SOA_TIMESTAMP = 2014090401
 NAMESERVERS = (
-    'ns1.pbsock.getpebble.com',
-    'ns2.pbsock.getpebble.com',
+    'ns1.pbsock.getpebble.com'
 )
 PORT = 10053
